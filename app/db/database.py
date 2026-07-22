43fc08995f8de665926671513ddf66e428a550f9
Petit-DJ <itssimplydjsuryansh@gmail.com>
2026-07-22 18:05:52 +0530

    added routes, models

---
+++ app/db/database.py
@@ -0,0 +1,16 @@
+#db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./ocr.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
+
+SessionLocal = sessionmaker(
+    autoflush=False,
+    autocommit=False,
+    bind=engine,
+)
